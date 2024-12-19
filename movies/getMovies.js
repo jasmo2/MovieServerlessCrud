8aa84767600fe9eefadf5ced40a7106bf25e8934
Jaime Molina <js.molina13@uniandes.edu.co>
2024-12-19 17:49:49 -0500

    getMovies handler

---
+++ movies/getMovies.js
@@ -0,0 +1,33 @@
+const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb")
+const { unmarshall } = require("@aws-sdk/util-dynamodb")
+
+const getMovie = async (event) => {
+  try {
+    const client = new DynamoDBClient()
+    const { id } = event.pathParameters
+
+    const command = new GetItemCommand({
+      TableName: "MoviesTable",
+      Key: {
+        id: { S: id },
+      },
+    })
+
+    const result = await client.send(command)
+    const movie = result.Item ? unmarshall(result.Item) : null
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(movie),
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    }
+  }
+}
+
+module.exports = {
+  getMovie,
+}
